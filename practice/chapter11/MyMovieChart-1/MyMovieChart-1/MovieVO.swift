9b8b1300fdd632611165adc141d2c7d033f23ec7
Hyungju LEE <hyungjuice@naver.com>
2022-05-17 20:38:57 +0900

    Feat: [#11] 추가 WKWebView이용 실습
    - WKWebView 이용 상세페이지 로딩
    - 델리게이트 패턴 이용한 웹 뷰의 로딩 처리

---
+++ practice/chapter11/MyMovieChart-1/MyMovieChart-1/MovieVO.swift
@@ -0,0 +1,20 @@
+//
+//  MovieVO.swift
+//  MyMovieChart-1
+//
+//  Created by 이형주 on 2022/05/12.
+//
+
+import Foundation
+import UIKit
+
+class MovieVO {
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var moreInfo: String?
+    var releaseDate: String?
+    var rating: Double?
+    
+    var thumbnailImage: UIImage?
+}
